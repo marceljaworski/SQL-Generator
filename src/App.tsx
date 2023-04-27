c8ae4eeb2e44603135811a1797b12569a69a3cbb
Marcelearn <jaworskimarcel@gmail.com>
2023-04-27 11:12:16 +0200

    getQuery

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,24 @@ import CodeDisplay from "./components/CodeDisplay";
 
 
 function App() {
+  const getQuery = async() => {
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          message: "create a table"
+        })
+      }
+      const response = await fetch("http://localhost:800/completions", options);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <div className="app">
@@ -10,7 +28,7 @@ function App() {
       <input type="text" />
       <CodeDisplay/>
       <div className="button-container">
-        <button id="get-query">Get Query!</button>
+        <button id="get-query" onClick={getQuery}>Get Query!</button>
         <button id="clear-chat">Clear Chat</button>
       </div>
     </div>
